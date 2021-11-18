914cd495ce1f6f80bca7c0fcd2a38533d01ea554
dev11 <dev11@dccolorweb.com>
2021-11-18 10:37:15 -0600

    :sparkles: adding echo

---
+++ app/Http/Controllers/userController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class userController extends Controller
+{
+     public function getUser() :void
+    {
+        echo "";
+
+    }
+}
