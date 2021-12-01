d6774e1ae5a8ed5cd53fb9e5926d0bf2038e2a92
dev11 <dev11@dccolorweb.com>
2021-12-01 15:08:44 -0600

    :sparkles: adding jsonresponse has return value

--- app/Http/Controllers/userController.php
+++ app/Http/Controllers/userController.php
@@ -4,10 +4,11 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Http\JsonResponse;
 
 class userController extends Controller
 {
-     public function getUser() : Response
+     public function getUser() : JsonResponse 
     {
         return response()->json(["msg"=>"hello"]);
     }
