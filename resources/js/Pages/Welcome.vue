cc6a6330ff9b4a24a064027cc0c5af2e31ccc3c4
g14wx <carlos.hernandez3@catolica.edu.sv>
2021-12-01 22:40:49 -0600

    :sparkles: :hammer: new title was added

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -19,7 +19,7 @@
         </div>
 
         <div class="max-w-6xl mx-auto sm:px-6 lg:px-8">
-            <div class="flex justify-center pt-8 sm:justify-start sm:pt-0">
+            <div class="flex justify-center pt-8 sm:justify-start sm:pt-0 text-white">
                 This is the new title
             </div>
 
