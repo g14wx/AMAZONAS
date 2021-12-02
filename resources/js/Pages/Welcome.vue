78208b369ceff28ce3d4a8934a290419c3d4fe34
ac-04 <60805064+shift14wx@users.noreply.github.com>
2021-12-01 22:42:46 -0600

    :sparkles: new title was added as requested (#31)
    * :sparkles: new title was added as requested
    
    * :sparkles: :hammer: new title was added
    
    Co-authored-by: g14wx <carlos.hernandez3@catolica.edu.sv>

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -19,7 +19,8 @@
         </div>
 
         <div class="max-w-6xl mx-auto sm:px-6 lg:px-8">
-            <div class="flex justify-center pt-8 sm:justify-start sm:pt-0">
+            <div class="flex justify-center pt-8 sm:justify-start sm:pt-0 text-white">
+                This is the new title
             </div>
 
             <div class="mt-8 bg-white dark:bg-gray-800 overflow-hidden shadow sm:rounded-lg">
