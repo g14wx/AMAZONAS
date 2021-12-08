8789f4f84905ef648c61c539b82a3b4149778841
g14wx <carlos.hernandez3@catolica.edu.sv>
2021-12-08 00:01:33 -0600

    Revert "AMAZONAS-0030-add-fancy-title-to-landing-page (#48)" reverting due some problems with this task This reverts commit 660d6e69a56e3ea58b552d4c892b655d6159cf0f.

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -20,7 +20,6 @@
 
         <div class="max-w-6xl mx-auto sm:px-6 lg:px-8">
             <div class="flex justify-center pt-8 sm:justify-start sm:pt-0">
-                <h1 class="text-9xl text-white">Hi welcome to amazonas</h1>
             </div>
 
             <div class="mt-8 bg-white dark:bg-gray-800 overflow-hidden shadow sm:rounded-lg">
