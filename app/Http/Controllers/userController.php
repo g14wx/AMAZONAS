39a95d8272ef191b0f9eb3f10c5af3482b57e535
negus <75600781+g14wx@users.noreply.github.com>
2021-12-21 15:45:19 -0600

    Revert "AMAZONAS-7777"
    This reverts commit 67d7aee3e553b5f085426d8f83101cc3d105988e.

--- app/Http/Controllers/userController.php
+++ app/Http/Controllers/userController.php
@@ -10,13 +10,4 @@ use Psy\Util\Json;
 
 class userController extends Controller
 {
-    public function hour() :JsonResponse
-    {
-        return response()->json(["hour"=>Carbon::today()->toString()]);
-    }
-
-    public function newusr() :JsonResponse
-    {
-        return response()->json(["hello user!"]);
-    }
 }
