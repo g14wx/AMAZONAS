12b185538ea13b6745cb94f4ef27f1a9108ceca7
negus <75600781+g14wx@users.noreply.github.com>
2022-08-07 16:05:38 -0600

    merge main to update stagging branch (#77)
    * adding template
    
    * :sparkles: replacing title
    
    * removing endpoint hour
    
    * Amazonas release 1 1 1 (#74)
    
    * AMAZONAS-7777
    
    * Added a new endpoint with a message
    
    * Modded the msg
    
    Co-authored-by: dev10-dccolorweb <dev10@dccolorweb.com>
    Co-authored-by: negus <75600781+g14wx@users.noreply.github.com>
    
    * AMAZONAS-8888 (#73)
    
    Co-authored-by: negus <75600781+g14wx@users.noreply.github.com>
    
    Co-authored-by: mrvictor22 <46722867+mrvictor22@users.noreply.github.com>
    Co-authored-by: dev10-dccolorweb <dev10@dccolorweb.com>
    Co-authored-by: goth14wx <goth14wx@gmail.com>
    
    * Revert 73 amazonas 8888 add endpoint returning hour (#75)
    
    * AMAZONAS-7777
    
    * Added a new endpoint with a message
    
    * Modded the msg
    
    Co-authored-by: dev10-dccolorweb <dev10@dccolorweb.com>
    Co-authored-by: negus <75600781+g14wx@users.noreply.github.com>
    
    * AMAZONAS-8888 (#73)
    
    Co-authored-by: negus <75600781+g14wx@users.noreply.github.com>
    
    * Revert "AMAZONAS-8888 (#73)"
    
    This reverts commit be5e92c8d312957a28da1123af8d5ea4bb23703e.
    
    Co-authored-by: mrvictor22 <46722867+mrvictor22@users.noreply.github.com>
    Co-authored-by: dev10-dccolorweb <dev10@dccolorweb.com>
    Co-authored-by: goth14wx <goth14wx@gmail.com>
    
    * Revert 72 amazonas 7777 add endpoint returning a hello new user (#76)
    
    * AMAZONAS-7777
    
    * Added a new endpoint with a message
    
    * Modded the msg
    
    Co-authored-by: dev10-dccolorweb <dev10@dccolorweb.com>
    Co-authored-by: negus <75600781+g14wx@users.noreply.github.com>
    
    * AMAZONAS-8888 (#73)
    
    Co-authored-by: negus <75600781+g14wx@users.noreply.github.com>
    
    * Revert "AMAZONAS-7777"
    
    This reverts commit 67d7aee3e553b5f085426d8f83101cc3d105988e.
    
    Co-authored-by: mrvictor22 <46722867+mrvictor22@users.noreply.github.com>
    Co-authored-by: dev10-dccolorweb <dev10@dccolorweb.com>
    Co-authored-by: goth14wx <goth14wx@gmail.com>
    
    * feat: Add some endpoints to support
    
    * fix: Add fix to pass check from phpStan
    
    * feat: Add dangerours faker to require in composer json
    
    * fix: Get rid of faker in production
    
    * fix: Add needed endpoints to mock server
    
    * fix: Add fix for show correct data in mock endpoints
    
    * fix: Add fix for show correct data in mock endpoints
    
    * fix: Add date format "Y-m-d"
    
    Co-authored-by: dev11 <dev11@dccolorweb.com>
    Co-authored-by: mrvictor22 <46722867+mrvictor22@users.noreply.github.com>
    Co-authored-by: dev10-dccolorweb <dev10@dccolorweb.com>
    Co-authored-by: goth14wx <goth14wx@gmail.com>
    Co-authored-by: dev11-dccolorweb <92184908+dev11-dccolorweb@users.noreply.github.com>
    Co-authored-by: g14wx <hdcj@protonmail.com>

--- app/Http/Controllers/userController.php
+++ app/Http/Controllers/userController.php
@@ -2,16 +2,138 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\User;
 use Carbon\Carbon;
-use Illuminate\Http\Request;
 use Illuminate\Http\JsonResponse;
-use Psy\Util\Json;
+use Illuminate\Support\Collection;
 
 class userController extends Controller
 {
-    public function hour() :JsonResponse
+    public function hour(): JsonResponse
     {
-        return response()->json(["hour"=>Carbon::today()->toString()]);
+        return response()->json(["hour" => Carbon::today()->toString()]);
+    }
+
+    public function newusr(): JsonResponse
+    {
+        return response()->json(["hello user!"]);
+    }
+
+    public function topAndTransaction()
+    {
+        $response = [
+            "top_selling_categories",
+            "top_selling_products",
+            "transaction_amount_per_visit"
+        ];
+        return $response;
+    }
+
+    public function subscriberGrowth(): JsonResponse
+    {
+        $titles = ["Total Active Subscribers", "Total Opt-ins", "Total Opt-outs"];
+        $titlesForSeries = ["Subscribers"];
+        $response = [
+            "cards" => $this->generateCards($titles),
+            "series" => $this->generateSeries($titlesForSeries, true)[0]
+        ];
+        return response()->json($response);
+    }
+
+    public function salesVsCampaign(): JsonResponse
+    {
+        $titles = ["Total Sales connected to campaigns", "Average daily sales"];
+        $titlesForSeries = ["Sales", "Text Campaigns", "Email Campaigns"];
+        $response = [
+            "informativeCards" => Collection::make($this->generateCards($titles))->map(function ($informativeCard) {
+                return [
+                    "title" => $informativeCard["title"],
+                    "data" => $informativeCard
+                ];
+            })->toArray(),
+            "series" => $this->generateSeries($titlesForSeries, true)
+        ];
+        return response()->json($response);
+    }
+
+    public function textMessageCampaignHealth(): JsonResponse
+    {
+        $titles = ["Total Campaigns", "Total Messages", "Delivery Rate", "CTR", "Opt-out Rate"];
+        $titlesForSeries = ["Total Messages", "Delivery Rate", "CTR", "Opt-out Rate"];
+        $response = [
+            "cards" => $this->generateCards($titles),
+            "series" => $this->generateSeries($titlesForSeries)
+        ];
+        return response()->json($response);
+    }
+
+    public function emailCampaignHealth(): JsonResponse
+    {
+        $titles = ["Total Campaigns", "Total Emails", "Delivery Rate", "Open Rate", "CTR", "Unsubscribe Rate"];
+        $titlesForSeries = ["Total Emails", "Delivery Rate", "Open Rate", "CTR", "Unsubscribe Rate"];
+        $response = [
+            "cards" => $this->generateCards($titles),
+            "series" => $this->generateSeries($titlesForSeries)
+        ];
+        return response()->json($response);
+    }
+
+    private function generateSeries(array $titles, bool $setDataToNumbers = false): array
+    {
+
+        $collection = Collection::make($titles);
+        $d1 = date_create(date('Y') . '-' . date('m') . '-01'); //current month/year
+        // @phpstan-ignore-next-line
+        $d2 = date_create($d1->format('Y-m-t')); //get last date of the month
+        // @phpstan-ignore-next-line
+        $daysObject = date_diff($d1, $d2);
+        $days = Collection::range(0, $daysObject->days + 1);
+        unset($titles[0]);
+        $series = $collection->map(function ($title) use ($days, $setDataToNumbers) {
+            return [
+                "title" => $title,
+                "data" => $days->map(function ($day) use ($setDataToNumbers) {
+                    return $setDataToNumbers ? mt_rand(1, 999999) : [
+                        "numberOfDay" => $day,
+                        "value" => mt_rand(1, 999999)
+                    ];
+                })->toArray(),
+                "subInformation" => $days->map(function ($day) use ($title, $setDataToNumbers) {
+                    return [
+                        "title" => $title,
+                        "date" => Carbon::now()->format("Y-m"). "-" . $day,
+                        "monthInformation" => Collection::make($setDataToNumbers ? [0, 1, 2] : [0, 1])->map(function ($number) use ($setDataToNumbers) {
+                            $monthInformation = [
+                                "title" => "some title",
+                                "value" => mt_rand(1, 999999),
+                                "isPercentageValue" => (mt_rand(1, 2) == 1)
+                            ];
+                            if ($number == 1 || $setDataToNumbers) {
+                                $monthInformation["isPositivePercentage"] = (mt_rand(1, 2) == 1);
+                                $monthInformation["percentage"] = mt_rand(1, 100);
+                            }
+                            return $monthInformation;
+                        })->toArray()
+                    ];
+                })->toArray()
+
+            ];
+        });
+        return $series->toArray();
+    }
+
+    private function generateCards(array $titles): array
+    {
+
+        $collection = Collection::make($titles);
+        $cards = $collection->map(function ($title) {
+            return [
+                "title" => $title,
+                "value" => mt_rand(1, 999999),
+                "percentage" => mt_rand(1, 100) . "%",
+                "isPositivePercentage" => (mt_rand(1, 2) == 1),
+                "isValuePercentage" => false
+            ];
+        });
+        return $cards->toArray();
     }
 }
