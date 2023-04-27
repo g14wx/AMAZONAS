f8efe4316258588906357d2030606584e7542212
g14wx <carlos.hernandez3@catolica.edu.sv>
2023-04-27 10:40:20 -0600

    fix(fix): Add a little change

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -27,7 +27,7 @@
                 <div class="grid grid-cols-1 md:grid-cols-2">
                     <div class="p-6">
                         <div class="flex items-center">
-                          <div class="ml-4 text-lg leading-7 font-semibold"><a href="https://laravel.com/docs" class="underline text-gray-900 dark:text-white">Documentation from shift14wx</a></div>
+                          <div class="ml-4 text-lg leading-7 font-semibold"><a href="https://laravel.com/docs" class="underline text-gray-900 dark:text-white">Documentation from the admin</a></div>
                         </div>
 
                         <div class="ml-12">
